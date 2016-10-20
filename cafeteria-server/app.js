ea46e7d160938fc53135618b5ae7bf4fb2d6523e
JFPereira <pereiraffjoao1993@gmail.com>
2016-10-20 01:54:12 +0100

    api now works but only in two gets

--- cafeteria-server/app.js
+++ cafeteria-server/app.js
@@ -1,14 +1,30 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var dotenv = require('dotenv');
+
+dotenv.load();
+
+var db = require('./cafeteria.js');
+var Product = require('./product.js')
 
 var app = express();
+
+var p = new Product('Joao', 1);
+var p2 = new Product('Carlos', 2);
+
+console.log(p.name);
+
+//db.startDB();
+
+//db.insertProduct(p);
+//db.insertProduct(p2);
 
 /** bodyParser.urlencoded(options)
  * Parses the text as URL encoded data (which is how browsers tend to send form data from regular forms set to POST)
  * and exposes the resulting object (containing the keys and values) on req.body
  */
 app.use(bodyParser.urlencoded({
-    extended: true
+	extended: true
 }));
 
 /**bodyParser.json(options)
@@ -16,9 +32,27 @@ app.use(bodyParser.urlencoded({
  */
 app.use(bodyParser.json());
 
+function callback(res, obj) {
+	res.json({
+		success: true,
+		data: obj
+	});
+}
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+	res.send('Hello World!');
+});
+
+app.get('/api/user', function (req, res) {
+	res.send('Ola user!');
+});
+
+app.get('/api/product/:name', function (req, res) {
+	db.getProductByName(req, res, callback);
+});
+
+app.get('/api/products', function (req, res) {
+	db.getProducts(res, callback);
 });
 
 app.listen(process.env.PORT || 5000);
--- cafeteria-server/cafeteria.js
+++ cafeteria-server/cafeteria.js
@@ -2,11 +2,11 @@ const pg = require('pg');
 
 function initClient() {
 	const client = new pg.Client({
-		user: 'fqhoczevushmzq',
-		password: 'dfza0Wcbr-TvWljhyzSUUBETbe',
-		database: 'd5lpdcg8qd6e9',
-		port: 5432,
-		host: 'ec2-54-217-233-209.eu-west-1.compute.amazonaws.com',
+		user: process.env.DATABASE_USER,
+		password: process.env.DATABASE_PASSWORD,
+		database: process.env.DATABASE_NAME,
+		port: process.env.DATABASE_PORT,
+		host: process.env.DATABASE_HOST,
 		ssl: true
 	});
 	return client;
@@ -17,6 +17,7 @@ function createTableUsers() {
 
 	client.connect();
 	const query = client.query(
+		'DROP TABLE IF EXISTS users;' +
 		'CREATE TABLE users (' +
 		'id UUID PRIMARY KEY DEFAULT gen_random_uuid(),'+
 		'name VARCHAR(120) not null, '+
@@ -26,4 +27,74 @@ function createTableUsers() {
 		'hash_pin TEXT not null)');
 
 	query.on('end', () => { client.end(); });
+}
+
+function createTableProducts() {
+	var client = initClient();
+
+	client.connect();
+	const query = client.query(
+		'DROP TABLE IF EXISTS products;' +
+		'CREATE TABLE products (' +
+		'id UUID PRIMARY KEY DEFAULT gen_random_uuid(),'+
+		'name VARCHAR(120) not null, '+
+		'price FLOAT not null)');
+
+	query.on('end', () => { client.end(); });
+}
+
+exports.insertProduct = function insertProduct(product){
+
+	var client = initClient();
+
+	client.connect();
+	const query = client.query('INSERT INTO products (name, price) VALUES ($1, $2) RETURNING *', [product.name, product.price],
+		function(err, result) {
+			if (err) {
+					console.log(err);
+			} else {
+					console.log('row inserted with id: ' + result.rows[0].id);
+			}
+		//done();
+	});
+	query.on('end', () => { client.end(); });
+}
+
+exports.getProductByName = function getProductByName(req, res, callback) {
+	var client = initClient();
+
+	var name = req.params['name'];
+
+	client.connect();
+	const query = client.query("SELECT * FROM products WHERE products.name='" + name +"'",
+		function(err, result) {
+			if (err) {
+					console.log(err);
+			} else {
+					callback(res, result.rows[0]);
+			}
+	});
+	query.on('end', () => { client.end(); });
+
+	return query;
+}
+
+exports.getProducts = function getProducts(res, callback) {
+	var client = initClient();
+
+	client.connect();
+	const query = client.query('SELECT * FROM products',
+		function(err, result) {
+			if (err) {
+					console.log(err);
+			} else {
+					callback(res, result.rows);
+			}
+	});
+	query.on('end', () => { client.end(); });
+}
+
+exports.startDB = function startDB() {
+	createTableUsers();
+	createTableProducts();
 }
