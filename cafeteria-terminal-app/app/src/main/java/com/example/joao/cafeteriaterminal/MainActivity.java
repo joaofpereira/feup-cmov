73cb5ac16c784c32c0ec35a8a6520fa4762820b7
José Cardoso <josecardos@gmail.com>
2016-10-28 22:06:38 +0100

    qr code reader done. Needs refactor and integration with app

--- cafeteria-terminal-app/app/src/main/java/com/example/joao/cafeteriaterminal/MainActivity.java
+++ cafeteria-terminal-app/app/src/main/java/com/example/joao/cafeteriaterminal/MainActivity.java
@@ -1,46 +1,33 @@
 package com.example.joao.cafeteriaterminal;
 
-import android.app.Activity;
-import android.app.AlertDialog;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import com.google.zxing.MultiFormatReader;
+import android.widget.Button;
+import com.example.joao.cafeteriaterminal.R;
 
-
-
-public class MainActivity extends Activity implements MultiFormatReader {
-    private ZXingScannerView mScannerView;
+public class MainActivity extends AppCompatActivity {
+    Button gen, scan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public void onClick(View v){
-        mScannerView = new ZXingScannerView(this);
-        setContentView(mScannerView);
-        mScannerView.setResultHandler(this);
-        mScannerView.startCamera();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        mScannerView.stopCamera();
-    }
-
-    @Override
-    public void handleResult(Result result) {
-        //Do anything with result here :D
-        Log.w("handleResult", result.getText());
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Scan result");
-        builder.setMessage(result.getText());
-        AlertDialog alertDialog = builder.create();
-        alertDialog.show();
-
-        //Resume scanning
-        //mScannerView.resumeCameraPreview(this);
+        gen = (Button) findViewById(R.id.gen);
+        scan = (Button) findViewById(R.id.scan);
+        gen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gIntent = new Intent(MainActivity.this, GeneratorActivity.class);
+                startActivity(gIntent);
+            }
+        });
+        scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent rIntent = new Intent(MainActivity.this, ReaderActivity.class);
+                startActivity(rIntent);
+            }
+        });
     }
 }
